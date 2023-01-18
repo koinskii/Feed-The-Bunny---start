25535da7ff6683262d810762a1fb571f018ba3ec
koinskii <andrekoinski8@gmail.com>
2023-01-17 21:05:52 -0300

    cut de rope

--- sketch.js
+++ sketch.js
@@ -14,6 +14,33 @@ var ground;
 var fruit
 var rope 
 var link
+var bgimg
+var fruitimg
+var bunny
+var eat
+var sad
+var blink
+var button1
+function preload() {
+  //carregando images
+  bgimg = loadImage("./images/background.png")
+  eat = loadAnimation("./images/eat_0.png","./images/eat_1.png","./images/eat_2.png","./images/eat_3.png","./images/eat_4.png")
+  sad = loadAnimation("./images/sad_1.png","./images/sad_2.png","./images/sad_3.png")
+  blink = loadAnimation("./images/blink_1.png","./images/blink_2.png","./images/blink_3.png")
+  fruitimg = loadImage("./images/melon.png")
+ 
+  //animação começar tocando
+  blink.playing = true
+  eat.playing = true
+  sad.playing = true
+
+  //loop da animação
+  sad.looping = false
+  eat.looping = false
+
+
+}
+
 
 function setup() {
   createCanvas(500, windowHeight-1);
@@ -27,10 +54,27 @@ function setup() {
 
   rope = new Rope(7,{x:150, y:50})
 
+  //botões
+  button1 = createImg("./images/cut_btn.png")
+  button1.size(50, 50)
+  button1.position(130, 50)
+  button1.mouseClicked(drop)
 
   Matter.Composite.add(rope.body,fruit)
 
   link = new Link(rope,fruit)
+
+//animações lentas
+  blink.frameDelay = 20
+  eat.frameDelay = 20
+  sad.frameDelay = 20
+
+  //Sprite do coelho
+  bunny = createSprite(170, height - 80, 100, 100)
+  bunny.scale = 0.2
+  bunny.addAnimation("blink", blink)
+  bunny.addAnimation("eat", eat)
+  bunny.addAnimation("sad", sad)
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -40,16 +84,23 @@ function setup() {
 
 function draw() {
   background(51);
+  image(bgimg, width/2, height/2, width, height)
   Engine.update(engine);
+
 
   ground.display()
   fill("yellow")
-  ellipse(fruit.position.x,fruit.position.y,20,20)
+  image(fruitimg,fruit.position.x,fruit.position.y,60,60)
   rope.show()
 
-
+  drawSprites();
 }
 
+function drop() {
+  rope.break()
+  link.detach()
 
+  link = null
+}
 
 
